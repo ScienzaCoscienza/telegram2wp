f81bef4b66ff9b7124a21aa9f33534fbab6a5838
ScienzaCoscienza <info@scienzacoscienza.com>
2023-07-15 16:37:02 +0200

    maurizioblondet.it added to black list

--- config/prod/settings.php
+++ config/prod/settings.php
@@ -4,6 +4,6 @@ defined('TELEGRAM_RSS_URL') OR define('TELEGRAM_RSS_URL', 'https://rsshub.app/te
 defined('FULL_TEXT_RSS_URL') OR define('FULL_TEXT_RSS_URL', 'https://bacheca.scienzacoscienza.com/full-text-rss');
 defined('WP_WEBSITE_TARGET_URL') OR define('WP_WEBSITE_TARGET_URL', 'https://bacheca.scienzacoscienza.com');
 defined('WP_PUBLISH_AS_DRAFT') OR define('WP_PUBLISH_AS_DRAFT', true);
-defined('DOMAIN_BLACK_LIST') OR define('DOMAIN_BLACK_LIST', ['renovatio21.com', 'voltairenet.org', 'thecradle.co', 'corrierecomunicazioni.it', 'paulcraigroberts.org', 'comedonchisciotte.org', 'mediasetinfinity.mediaset.it']);
+defined('DOMAIN_BLACK_LIST') OR define('DOMAIN_BLACK_LIST', ['renovatio21.com', 'voltairenet.org', 'thecradle.co', 'corrierecomunicazioni.it', 'paulcraigroberts.org', 'comedonchisciotte.org', 'mediasetinfinity.mediaset.it', 'maurizioblondet.it']);
 defined('AI_SEARCH_API_URL') OR define('AI_SEARCH_API_URL', 'https://api.botheory.com/openai/search/');
 defined('AI_CHAT_API_URL') OR define('AI_CHAT_API_URL', 'https://api.botheory.com/openai/chat/');
